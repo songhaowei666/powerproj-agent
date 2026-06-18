2f9fdc11c2b8e65cf4c14016af8df6de377c0747
song <shwlife@163.com>
2026-06-18 16:42:14 +0800

    还是采用sstreamlit页面

--- web/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Web 前端模块，提供基于 React + FastAPI 的主控 Agent 聊天界面。"""
--- web/frontend/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import { Layout } from "./components/Layout";
-import "./styles/components.css";
-
-export function App() {
-  return <Layout />;
-}
